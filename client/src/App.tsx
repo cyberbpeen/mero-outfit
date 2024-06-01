7b9f646399d868df6f51ff3f3381625b70419ada
Bipin Mahato <cyberbpeen@gmail.com>
2024-06-01 09:05:37 +0545

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Button>Hello World!!!</Button>
+    </div>
+  );
+}
+
+export default App;
